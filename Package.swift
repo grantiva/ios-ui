b8718dfb71ef04d353f76ed6bd39bda6bcd59351
Kyle Browning <kylebrowning@me.com>
2026-05-10 06:40:41 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/grantiva/ios-sdk.git", from: "1.0.1"),
+        .package(url: "https://github.com/grantiva/ios-sdk.git", from: "2.0.1"),
     ],
     targets: [
         .target(
